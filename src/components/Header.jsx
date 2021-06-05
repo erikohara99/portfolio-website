9d6c1d9ab9a29bcc0198f91e6db1aba7482a00ac
Erik <slicedblue@gmail.com>
2021-06-04 20:14:49 -0400

    edited link colors at top, fixed splash background render issues, changed button color to blue, shortened border radius

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ class Header extends Component {
                 <p>Erik O'Hara</p>
                 <div id = "links">
                     <ul>
-                        <li><a href="https://github.com/SlicedBlu">GitHub</a></li>
-                        <li><a href="https://www.linkedin.com/in/erik-ohara/">LinkedIn</a></li>
-                        <li><a href="https://drive.google.com/file/d/1JKiEBVlSm4D8q1ApK6JpFTaj-cuLk_bp/view">Resume</a></li>
+                        <li><a href="https://github.com/SlicedBlu"><span>GitHub</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/erik-ohara/"><span>LinkedIn</span></a></li>
+                        <li><a href="https://drive.google.com/file/d/1JKiEBVlSm4D8q1ApK6JpFTaj-cuLk_bp/view"><span>Resume</span></a></li>
                     </ul>
                 </div>
                 <p id="button">Contact Me</p>
