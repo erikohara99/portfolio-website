106a5cf962555e225a26f136f51b2b4afa2211c0
Erik <slicedblue@gmail.com>
2021-09-10 21:21:56 -0400

    fixed title of site, edited github links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ class Footer extends Component {
                 <p>Erik O'Hara</p>
                 <div id = "links">
                     <ul>
-                        <li><a href="https://github.com/SlicedBlu"><span>GitHub</span></a></li>
+                        <li><a href="https://github.com/erikohara99"><span>GitHub</span></a></li>
                         <li><a href="https://www.linkedin.com/in/erik-ohara/"><span>LinkedIn</span></a></li>
-                        <li><a href="https://drive.google.com/file/d/1JKiEBVlSm4D8q1ApK6JpFTaj-cuLk_bp/view"><span>Resume</span></a></li>
+                        <li><a href="https://drive.google.com/file/d/1NVgyEZB2qbunOueyb6lqa43xPT50_lbo/view?usp=sharing"><span>Resume</span></a></li>
                     </ul>
                 </div>
                 <a href="mailto:erikmohara@gmail.com" id="button">Contact Me</a>
